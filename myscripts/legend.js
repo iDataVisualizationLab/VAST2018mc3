a9859ccdc878b311d1f80f16634304990d5a5ebb
Tommy Dang <nhontuan@gmail.com>
2018-06-23 20:12:19 -0500

    update legends

---
+++ myscripts/legend.js
@@ -0,0 +1,188 @@
+/* June, 2018
+ * Tommy Dang (on the Scagnostics project, as Assistant professor, iDVL@TTU)
+ *
+ * THIS SOFTWARE IS BEING PROVIDED "AS IS", WITHOUT ANY EXPRESS OR IMPLIED
+ * WARRANTY.  IN PARTICULAR, THE AUTHORS MAKE NO REPRESENTATION OR WARRANTY OF ANY KIND CONCERNING THE MERCHANTABILITY
+ * OF THIS SOFTWARE OR ITS FITNESS FOR ANY PARTICULAR PURPOSE.
+ */
+
+var transactions = ["Call", "Email", "Meeting", "Purchase"];
+var transCounts = [0, 0, 0, 0];
+
+var clickCount1 =0;
+
+function colores_google(n) {
+    var colores_g = ["#3060aa", "#660099", "#109618", "#996600", "#990099", "#0099c6", "#dd4477", "#66aa00", "#b82e2e", "#316395", "#994499", "#22aa99", "#aaaa11", "#6633cc", "#e67300", "#8b0707", "#651067", "#329262", "#5574a6", "#3b3eac"];
+    return colores_g[n % colores_g.length];
+}
+
+function drawLegends(){
+
+    var svgLegend = d3.select("#controlPanel")
+        .append("svg")
+        .attr("width", 200)
+        .attr("height",800);
+
+    var legendTop1 = 70;
+    svgLegend.append("text")
+        .attr("class","textTitle1")
+        .attr("x", 8 )
+        .attr("y", legendTop1)
+        .attr("fill", "#000" )
+        .text("Nodes (or people)");
+
+    svgLegend.append("text")
+        .attr("class","textLegend1")
+        .attr("x", 30 )
+        .attr("y", legendTop1+20)
+        .attr("fill", "#a00" )
+        .text("Suspicious");
+    svgLegend.append("text")
+        .attr("class","textLegend1")
+        .attr("x", 30 )
+        .attr("y", legendTop1+40)
+        .attr("fill", "#444" )
+        .text("Related");
+
+
+
+
+
+    var legendTop2 = 150;
+    var scale = d3.scale.linear().domain([-1, 10]).range([legendTop2, legendTop2+180]);
+
+    svgLegend.append("text")
+        .attr("class","textTitle2")
+        .attr("x", 8 )
+        .attr("y", legendTop2)
+        .attr("fill", "#000" )
+        .text("Links (or actions)");
+
+    svgLegend.selectAll(".lineLegend2")
+        .data( d3.range(4) )
+        .enter().append("line")
+        .attr("class","lineLegend2")
+        .attr("x1", 8 )
+        .attr("y1", scale )
+        .attr("x2", 30 )
+        .attr("y2", scale )
+        .attr("stroke", function(d,i) { return colores_google(i); } )
+        .attr("stroke-width",2);
+
+    // Count the number of transactions in each category
+    for (var j=0; j<links.length;j++){
+        if (links[j].category =="0"){
+            transCounts[0]++;
+        }
+        else if (links[j].category =="1"){
+            transCounts[1]++;
+        }
+        else if (links[j].category =="3"){  // Meetings
+            transCounts[2]++;
+        }
+        else if (links[j].category =="2"){  // Purchases
+            transCounts[3]++;
+        }
+        else{
+            console.error("Weird category!!!!");
+        }
+    }
+
+    svgLegend.selectAll(".textLegend2")
+        .data(d3.range(4) )
+        .enter()
+        .append("text")
+        .attr("class","textLegend2")
+        .attr("x", 33 )
+        .attr("y", scale )
+        .attr("dy", 5 )
+        .attr("fill", function(d,i) { return colores_google(i); } )
+        .text(function(d,i) {
+          return transactions[i] +" ("+transCounts[i]+")";});
+
+
+}
+
+function buttonClick1(){
+    if (clickCount1%2==0){  // Show timeline *********
+        detactTimeSeries();
+    }
+    else{
+
+    }
+    clickCount1++;
+}
+
+function detactTimeSeries(){
+    // Stop force layout first
+    force.stop();
+
+    var array = [];
+    for (var i=0; i< nodes.length; i++) {
+        var e =  {};
+        e.y = nodes[i].y;
+        e.nodeId = i;
+        array.push(e);
+    }
+    array.sort(function (a, b) {
+        if (a.y > b.y) {
+            return 1;
+        }
+        if (a.y < b.y) {
+            return -1;
+        }
+        return 0;
+    });
+
+    var step = height/array.length;
+    for (var i=0; i< array.length; i++) {
+        nodes[array[i].nodeId].y = i*step;
+    }
+    force.stop();
+
+    updateTransition(2000, height-4);
+}
+
+function updateTransition(durationTime, timeY){  // timeY is the position of time legend
+    nodes.forEach(function(d) {
+        d.x=xScale(d.listTimes[0]);
+    });
+
+    nodeG.transition().duration(durationTime)
+        .attr('cx', function(d) { return d.x; })
+        .attr('cy', function(d) { return d.y; });
+  /*
+
+   svg.selectAll(".linePNodes").transition().duration(durationTime)
+   .attr("x1", function(d) {return xStep+xScale(d.minY);})
+   .attr("y1", function(d) {return d.y;})
+   .attr("x2", function(d) {return xStep+xScale(d.maxY);})
+   .attr("y2", function(d) {return d.y;});
+
+
+
+   svg.selectAll(".nodeText").transition().duration(durationTime)
+   .text(function(d) { return d.name; })
+   .attr("dy", "3px");
+
+   svg.selectAll(".nodeLine").transition().duration(durationTime)
+   .attr("x1", function(d) {return xStep+d.x;})
+   .attr("y1", function(d) {return d.y;})
+   .attr("x2", function(d) {return xStep+d.x+xScale(70);})
+   .attr("y2", function(d) {return d.y;});
+
+   */
+  /*
+   svg.selectAll(".layer").transition().duration(durationTime)
+   .attr("d", function(d) {
+   for (var m=numYear-1; m>=0; m--){
+   d.yearly[m].yNode = d.y;     // Copy node y coordinate
+   // if (d.yearly[m].value==0)
+   //     d.yearly.splice(m,1);
+   }
+   return area(d.yearly); }) ;
+   */
+
+    linkArcs.transition().duration(durationTime).attr("d", linkArc2);
+}
+
--- myscripts/main.js
+++ myscripts/main.js
@@ -1,3 +1,12 @@
+/* June, 2018
+ * Tommy Dang (on the Scagnostics project, as Assistant professor, iDVL@TTU)
+ *
+ * THIS SOFTWARE IS BEING PROVIDED "AS IS", WITHOUT ANY EXPRESS OR IMPLIED
+ * WARRANTY.  IN PARTICULAR, THE AUTHORS MAKE NO REPRESENTATION OR WARRANTY OF ANY KIND CONCERNING THE MERCHANTABILITY
+ * OF THIS SOFTWARE OR ITS FITNESS FOR ANY PARTICULAR PURPOSE.
+ */
+
+
 //Constants for the SVG
 var margin = {top: 0, right: 0, bottom: 5, left: 15};
 var width = document.body.clientWidth - margin.left - margin.right;
@@ -57,10 +66,7 @@ var minT=1000000, maxT=0;
 var suspicious = {};
 
 
-function colores_google(n) {
-    var colores_g = ["#3060aa", "#660099", "#996600", "#109618", "#990099", "#0099c6", "#dd4477", "#66aa00", "#b82e2e", "#316395", "#994499", "#22aa99", "#aaaa11", "#6633cc", "#e67300", "#8b0707", "#651067", "#329262", "#5574a6", "#3b3eac"];
-    return colores_g[n % colores_g.length];
-}
+
 
 d3.csv("data/involvedCompanyIndex.csv", function(error, data1) {
     data1.forEach(function (d) {
@@ -146,28 +152,12 @@ d3.csv("data/involvedCompanyIndex.csv", function(error, data1) {
 
 
         force.on("tick", function () {
-            update();
-        });
-        force.on("end", function () {
-            detactTimeSeries();
+            tick();
         });
 
-        setupSliderScale(svg);
-        drawColorLegend();
-        //drawTimeLegend();
 
-        var svg1b = d3.select("#g10c")
-            .append("svg")
-            .attr("width", 400)
-            .attr("height", 50);
-        svg1b.selectAll("circle")
-            .data( d3.range(4) )
-            .enter()
-            .append("circle")
-            .attr("r", 18 )
-            .attr("cx", d3.scale.linear().domain([-1, 10]).range([0, 400]) )
-            .attr("cy", 25)
-            .attr("fill", function(d,i) { return colores_google(i); } );
+        // Other functions *************************
+        drawLegends();
 
         /*for (var i = 0; i < termArray.length; i++) {
          optArray.push(termArray[i].term);
@@ -184,172 +174,99 @@ d3.csv("data/involvedCompanyIndex.csv", function(error, data1) {
 
 
 
-    function computeLinks() {
-        //Create all the line svgs but without locations yet
-        svg.selectAll(".linkArc").remove();
-        linkArcs = svg.append("g").selectAll("path")
-            .data(links)
-            .enter().append("path")
-            .attr("class", "linkArc")
-            .style("stroke", function (d) {
-                    return colores_google(d.category);
-            })
-            .style("stroke-opacity", 0.3)
-            .style("stroke-width", function (d) {
-                return 2;
-            });
+function computeLinks() {
+    //Create all the line svgs but without locations yet
+    svg.selectAll(".linkArc").remove();
+    linkArcs = svg.append("g").selectAll("path")
+        .data(links)
+        .enter().append("path")
+        .attr("class", "linkArc")
+        .style("stroke", function (d) {
+                return colores_google(d.category);
+        })
+        .style("stroke-opacity", 0.3)
+        .style("stroke-width", function (d) {
+            return 2;
+        });
 //            .on('mouseover', mouseoveredLink)
 //            .on('mouseout', mouseoutedLink);
 
 
 
-        svg.selectAll(".nodeG").remove();
-        nodeG = svg.selectAll(".nodeG")
-            .data(nodes).enter().append("circle")
-            .attr("class", "nodeG")
-            .attr('r', function (d) { return suspicious[d.id] ? 6 : 3;})
-            .attr('cx', function(d) { return d.x; })
-            .attr('cy', function(d) { return d.y; })
-            .attr("fill", function (d) { return suspicious[d.id] ? "#a00" : "#444"})
-            .attr("fill-opacity", 1)
-            .attr("stroke", "#fff")
-            .attr("stroke-opacity", 1);;
+    svg.selectAll(".nodeG").remove();
+    nodeG = svg.selectAll(".nodeG")
+        .data(nodes).enter().append("circle")
+        .attr("class", "nodeG")
+        .attr('r', function (d) { return suspicious[d.id] ? 6 : 3;})
+        .attr('cx', function(d) { return d.x; })
+        .attr('cy', function(d) { return d.y; })
+        .attr("fill", function (d) { return suspicious[d.id] ? "#a00" : "#444"})
+        .attr("fill-opacity", 1)
+        .attr("stroke", "#fff")
+        .attr("stroke-opacity", 1);;
 
-        /*
-        nodeG.append("text")
-            .attr("class", "nodeText")  
-            .text(function(d) { return d.name })           
-            .attr("dy", "3px")
-            .style("fill","#000000")
-            .style("text-anchor","end")
-            .style("text-shadow", "1px 1px 0 rgba(255, 255, 255, 0.6")
-            .style("font-weight", function(d) { return d.isSearchTerm ? "bold" : ""; })
-            .attr("font-family", "sans-serif")
-            .attr("font-size", "14px");
-        */
+    /*
+    nodeG.append("text")
+        .attr("class", "nodeText")
+        .text(function(d) { return d.name })
+        .attr("dy", "3px")
+        .style("fill","#000000")
+        .style("text-anchor","end")
+        .style("text-shadow", "1px 1px 0 rgba(255, 255, 255, 0.6")
+        .style("font-weight", function(d) { return d.isSearchTerm ? "bold" : ""; })
+        .attr("font-family", "sans-serif")
+        .attr("font-size", "14px");
+    */
 
-        svg.selectAll(".nodeLine").remove();
-        svg.selectAll(".nodeLine")
-            .data(nodes).enter()
-            .append("line")
-            .attr("class", "nodeLine")
-            .attr("x1", function(d) { return d.x; })
-            .attr("y1", function(d) { return d.y; })
-            .attr("x2", function(d) { return d.x+xScale(70); })
-            .attr("y2", function(d) { return d.y; })
-            .style("stroke-width",0.5)
-            .style("stroke-opacity",0.5)
-            .style("stroke", "#000");
+    /*
+    svg.selectAll(".nodeLine").remove();
+    svg.selectAll(".nodeLine")
+        .data(nodes).enter()
+        .append("line")
+        .attr("class", "nodeLine")
+        .attr("x1", function(d) { return d.x; })
+        .attr("y1", function(d) { return d.y; })
+        .attr("x2", function(d) { return d.x+xScale(70); })
+        .attr("y2", function(d) { return d.y; })
+        .style("stroke-width",0.5)
+        .style("stroke-opacity",0.5)
+        .style("stroke", "#000");
+    */
 
-        // Horizontal lines
-     /*   svg.selectAll(".linePNodes").remove();
-        linePNodes = svg.selectAll(".linePNodes")
-            .data(pNodes).enter().append("line")
-            .attr("class", "linePNodes")
-            .attr("x1", function(d) {return xStep+xScale(d.minY);})
-            .attr("y1", function(d) {return d.y;})
-            .attr("x2", function(d) {return xStep+xScale(d.maxY);})
-            .attr("y2", function(d) {return d.y;})
-            .style("stroke-dasharray", ("1, 1"))
-            .style("stroke-width",0.4)
-            .style("stroke", "#000");*/
-    }    
+    // Horizontal lines
+ /*   svg.selectAll(".linePNodes").remove();
+    linePNodes = svg.selectAll(".linePNodes")
+        .data(pNodes).enter().append("line")
+        .attr("class", "linePNodes")
+        .attr("x1", function(d) {return xStep+xScale(d.minY);})
+        .attr("y1", function(d) {return d.y;})
+        .attr("x2", function(d) {return xStep+xScale(d.maxY);})
+        .attr("y2", function(d) {return d.y;})
+        .style("stroke-dasharray", ("1, 1"))
+        .style("stroke-width",0.4)
+        .style("stroke", "#000");*/
+}
 
-
-
-
-    function update(){
-        nodes.forEach(function(d) {
-         //   d.x += (width/2-d.x)*0.1;
-        });
-
-
-    //    linkArcs.style("stroke-width", 0);
-        svg.selectAll(".nodeLine")
-            .style("stroke-opacity" , 0.01);
-
-        nodeG.attr('cx', function(d) { return d.x; })
-            .attr('cy', function(d) { return d.y; });
-
-        yScale = d3.scale.linear()
-        .range([0, 2])
-        .domain([0, 10]);
-
-        linkArcs.attr("d", linkArc);
-      //  if (force.alpha()<0.02)
-      //     force.stop();
-    } 
-
-    function updateTransition(durationTime, timeY){  // timeY is the position of time legend
-        nodes.forEach(function(d) {
-           d.x=xScale(d.listTimes[0]);
-        });
-
-        nodeG.transition().duration(durationTime)
-            .attr('cx', function(d) { return d.x; })
-            .attr('cy', function(d) { return d.y; });
-        /*
-        
-        svg.selectAll(".linePNodes").transition().duration(durationTime)
-            .attr("x1", function(d) {return xStep+xScale(d.minY);})
-            .attr("y1", function(d) {return d.y;})
-            .attr("x2", function(d) {return xStep+xScale(d.maxY);})
-            .attr("y2", function(d) {return d.y;}); 
+function tick(){
+    nodes.forEach(function(d) {
+     //   d.x += (width/2-d.x)*0.1;
+    });
 
 
-        
-        svg.selectAll(".nodeText").transition().duration(durationTime)
-            .text(function(d) { return d.name; })
-            .attr("dy", "3px"); 
+//    linkArcs.style("stroke-width", 0);
+    svg.selectAll(".nodeLine")
+        .style("stroke-opacity" , 0.01);
 
-        svg.selectAll(".nodeLine").transition().duration(durationTime)
-            .attr("x1", function(d) {return xStep+d.x;})
-            .attr("y1", function(d) {return d.y;})
-            .attr("x2", function(d) {return xStep+d.x+xScale(70);})
-            .attr("y2", function(d) {return d.y;});          
+    nodeG.attr('cx', function(d) { return d.x; })
+        .attr('cy', function(d) { return d.y; });
 
-        */
-        /*
-        svg.selectAll(".layer").transition().duration(durationTime)
-          .attr("d", function(d) { 
-            for (var m=numYear-1; m>=0; m--){
-                d.yearly[m].yNode = d.y;     // Copy node y coordinate
-               // if (d.yearly[m].value==0)
-               //     d.yearly.splice(m,1);
-            }
-        return area(d.yearly); }) ;
-        */
+    yScale = d3.scale.linear()
+    .range([0, 2])
+    .domain([0, 10]);
 
-        linkArcs.transition().duration(durationTime).attr("d", linkArc2);
-    }    
+    linkArcs.attr("d", linkArc);
+}
 
-    function detactTimeSeries(){
-        var array = [];
-        for (var i=0; i< nodes.length; i++) {
-            var e =  {};
-            e.y = nodes[i].y;
-            e.nodeId = i;
-            array.push(e);
-        }
-        array.sort(function (a, b) {
-          if (a.y > b.y) {
-            return 1;
-          }
-          if (a.y < b.y) {
-            return -1;
-          }
-          return 0;
-        });  
-
-        var step = height/array.length;
-        var totalH = array.length*step;
-        for (var i=0; i< array.length; i++) {
-            nodes[array[i].nodeId].y = i*step;
-        }
-        force.stop();
-
-        updateTransition(2000, height-4);
-    }
 
 function linkArc(d) {
     var dx = d.target.x - d.source.x,
